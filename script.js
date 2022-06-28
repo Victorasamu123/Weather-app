125d5e66719bcc1512f7672055e3021798a19d67
Asamu victor <asamuvictor03@gmail.com>
2022-06-29 00:33:50 +0100

    completed the functionality

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let cityName = document.getElementById("cityId").value;
-
-async function checkWeather(){
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${userLocation}&appid=09756d60b2011a99fb389f6e43e766cc`
-    console .log(url)
+async function getUrl(){
+    let cityName = document.getElementById("city").value;
+    let tempCity = document.getElementById("temp");
+    let presCity =  document.getElementById("presure");
+    let humCity =  document.getElementById("humdity");
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=09756d60b2011a99fb389f6e43e766cc`;
+    const response = await fetch(url);
+    const getResponse = await response.json();
+    tempCity.innerHTML = JSON.stringify(Math.round(getResponse.main.temp-273)+" °c");
+    presCity.innerHTML = JSON.stringify(getResponse.main.pressure);
+    humCity.innerHTML = JSON.stringify(getResponse.main.humidity);
+    console.log(getResponse);
 }
