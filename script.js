66b4668da4c7a58d41a3cfd82d1e25ebef0b0648
Asamu victor <asamuvictor03@gmail.com>
2022-06-28 22:55:32 +0100

    added input and button

---
+++ script.js
@@ -0,0 +1,6 @@
+let cityName = document.getElementById("cityId").value;
+
+async function checkWeather(){
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${userLocation}&appid=09756d60b2011a99fb389f6e43e766cc`
+    console .log(url)
+}
