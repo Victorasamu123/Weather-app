321e33f079451c5642aabfa7bcf13b487266a883
Asamu victor <asamuvictor03@gmail.com>
2022-06-29 01:33:14 +0100

    added new features to js

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ async function getUrl(){
     let cityName = document.getElementById("city").value;
     let tempCity = document.getElementById("temp");
     let presCity =  document.getElementById("presure");
-    let humCity =  document.getElementById("humdity");
+    let humCity =  document.getElementById("humidity");
+    let long = document.getElementById("lon");
+    let latt = document.getElementById("lat");
+    let county = document.getElementById("con");
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=09756d60b2011a99fb389f6e43e766cc`;
     const response = await fetch(url);
     const getResponse = await response.json();
-    tempCity.innerHTML = JSON.stringify(Math.round(getResponse.main.temp-273)+" °c");
-    presCity.innerHTML = JSON.stringify(getResponse.main.pressure);
-    humCity.innerHTML = JSON.stringify(getResponse.main.humidity);
+    tempCity.innerHTML = JSON.stringify(`${Math.round(getResponse.main.temp-273)} °c `);
+    presCity.innerHTML = JSON.stringify("Pressure: " +getResponse.main.pressure);
+    humCity.innerHTML = JSON.stringify("Humidity: " +getResponse.main.humidity +"%");
+    long.innerHTML = JSON.stringify("Longitude: "+ getResponse.coord.lon);
+    latt.innerHTML = JSON.stringify("Latitude: " +getResponse.coord.lat);
+    county.innerHTML = JSON.stringify("Country: " +getResponse.sys.country);
     console.log(getResponse);
 }
