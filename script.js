41d848a3a0051daa1822ed709c665985051c5876
Asamu victor <asamuvictor03@gmail.com>
2022-06-29 12:25:51 +0100

    styled the weather app

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ async function getUrl(){
     let long = document.getElementById("lon");
     let latt = document.getElementById("lat");
     let county = document.getElementById("con");
+    let townName = document.getElementById("town")
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=09756d60b2011a99fb389f6e43e766cc`;
     const response = await fetch(url);
     const getResponse = await response.json();
@@ -15,5 +16,6 @@ async function getUrl(){
     long.innerHTML = JSON.stringify("Longitude: "+ getResponse.coord.lon);
     latt.innerHTML = JSON.stringify("Latitude: " +getResponse.coord.lat);
     county.innerHTML = JSON.stringify("Country: " +getResponse.sys.country);
+    townName.innerHTML = JSON.stringify(getResponse.name);
     console.log(getResponse);
 }
